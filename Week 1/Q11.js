ee01f9ff7c1d64ca79597d77b8e4f7c518f62560
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-07-26 13:20:16 +0530

    Array + Loop Problem

---
+++ Week 1/Q11.js
@@ -0,0 +1,9 @@
+// print all even number in the array using loop
+
+let arr = [106, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i]);
+    }
+}
