0b40f98942834ac6d3bd79b6f739230d13da3ee3
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-07-26 12:51:39 +0530

    #2 checking if the number is even or odd [loops under function]

---
+++ Week 1/Q2.js
@@ -0,0 +1,15 @@
+// #2 checking if the number is even or odd [loops under function]
+
+function isEvenOdd(num){
+    let rem = num % 2;
+
+    if(rem == 0){
+      console.log("number is even")  
+    }
+
+    else {
+        console.log("number is odd")
+    }
+}
+
+isEvenOdd(9)
