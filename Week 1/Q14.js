557a998602b9aab63b550cb021fdeee3ad37b74e
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-07-26 19:57:02 +0530

    Write a function that returns the total number of negative numbers in an array

--- Week 1/Q14.js
+++ Week 1/Q14.js
@@ -2,7 +2,7 @@
 
 function findNegative(){
     let count = 0;
-    for(let i=0; i< arr.length; i++){
+    for(let i = 0; i < arr.length; i++){
         if(arr[i] < 0 ){
             count++;
         }
@@ -12,7 +12,7 @@ function findNegative(){
 
 }
 
-let arr =[1,-2, 3,-4, 5,-6, 7,-8, 9,-10]
+let arr = [1,-2, 3,-4, 5,-6, 7,-8, 9,-10]
 
 let count = findNegative(arr)
 console.log(count)
