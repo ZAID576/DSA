6afe7df6986ca8f9bdaa68374aaed2b140ff3e5c
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-07-30 23:12:59 +0530

    q15

---
+++ Week 1/Q15.js
@@ -0,0 +1,12 @@
+// write a function that return the largest number in the array
+
+function largestNumber(){
+    for (let i = 0; i<arr.length; i++)
+      
+}
+
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+largestNumber(arr)
