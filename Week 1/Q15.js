f02e99e88b784fb3d2677c3bd1e297bdf53b6278
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-01 23:57:44 +0530

    adding first and last element of array

--- Week 1/Q15.js
+++ Week 1/Q15.js
@@ -1,12 +1,17 @@
 // write a function that return the largest number in the array
 
 function largestNumber(){
+    let arr1 = []
     for (let i = 0; i<arr.length; i++)
-      
+
+    // let tem = 0;
+    // tem = arr[i] + arr[arr.length-1-i]
+
+    
+
 }
 
 
 
 let arr = [1,2,3,4,5,6,7,8,9,10]
-
-largestNumber(arr)
+console.log(arr.length);
